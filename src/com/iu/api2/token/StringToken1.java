25544e6f30525ee82d2ad2f354c24597f9fde1dc
dongdongdongk <dhk9309@gmail.com>
2023-01-09 15:29:20 +0900

    api

---
+++ src/com/iu/api2/token/StringToken1.java
@@ -0,0 +1,24 @@
+package com.iu.api2.token;
+
+import java.util.StringTokenizer;
+
+public class StringToken1 {
+
+	public static void main(String[] args) {
+		String nations = "Korea,서울,USA,워싱턴,UK,런던,Japan,도쿄";
+		String [] n = nations.split(",");
+		
+		StringTokenizer st = new StringTokenizer(nations, ",");
+		
+		while(st.hasMoreTokens()) {
+			NationDTO nationDTO = new NationDTO();
+			
+			String t1 = st.nextToken();
+			nationDTO.setName(t1);
+			
+			String t2 = st.nextToken();
+			nationDTO.setCity(t2);
+		}
+		
+	}
+}
