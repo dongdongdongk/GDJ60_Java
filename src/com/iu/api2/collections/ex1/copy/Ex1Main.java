90cb80c086ae04f1ee918ef331d51693d7bb6705
dongdongdongk <dhk9309@gmail.com>
2023-01-10 14:04:58 +0900

    api

--- src/com/iu/api2/collections/ex1/copy/Ex1Main.java
+++ src/com/iu/api2/collections/ex1/copy/Ex1Main.java
@@ -1,5 +1,7 @@
 package com.iu.api2.collections.ex1.copy;
 
+import java.util.ArrayList;
+
 public class Ex1Main {
 
 	public static void main(String[] args) {
@@ -9,9 +11,18 @@ public class Ex1Main {
 		//3. 학생정보 검색조회(이름)
 		//4. 학생정보추가
 		//5. 학생정보삭제(이름)
+		//프로그램 시작을 메인에서 하지말고 컨트롤러 만들고 초기화 등은 DAO 출력은 view 에서
+		StudentDAO dao = new StudentDAO();
+		ArrayList<StudentDTO> ar = dao.intit();
 		
-		StudentDAO studentDAO = new StudentDAO();
-		studentDAO.init();
-		//프로그램 시작을 메인에서 하지말고 컨트롤러 만들고 초기화 등은 DAO 출력은 view 에서 
+		StudentDTO studentDTO= dao.findByName(ar);
+		System.out.println(studentDTO.getName());
+		System.out.println(studentDTO.getAvg());
+		
+		//		//향상된 for
+//		for(StudentDTO studentDTO : ar) {
+//			System.out.println(studentDTO);
+//		}
+//		
 	}
 }
